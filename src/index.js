0297e446949c382d61f9d71f9c5b7b98c2e782b8
Yamboy1 <yamboyd1@gmail.com>
2022-10-21 16:31:38 +1300

    Quality of life for the package.json, and add local server

--- src/index.js
+++ src/index.js
@@ -10,21 +10,25 @@ import { pongCommand } from "./pong.js";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
-app.post(
-  "/interactions",
-  verifyKeyMiddleware(process.env.DISCORD_PUBLIC_KEY),
-  async (req, res) => {
-    const message = req.body;
-    if (message.type === InteractionType.APPLICATION_COMMAND) {
-      let a;
-      res.send(await pongCommand());
+if (process.env.DISCORD_PUBLIC_KEY != undefined) {
+  app.post(
+    "/interactions",
+    verifyKeyMiddleware(process.env.DISCORD_PUBLIC_KEY),
+    async (req, res) => {
+      const message = req.body;
+      if (message.type === InteractionType.APPLICATION_COMMAND) {
+        res.send(await pongCommand());
+      }
     }
-  }
-);
+  );
+} else {
+  console.warn(
+    "DISCORD_PUBLIC_KEY environment variable not found, running local dev server."
+  );
+  app.get("/", async (req, res) => {
+    res.send(await pongCommand());
+  });
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}.`);
